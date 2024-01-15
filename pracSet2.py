22058c9e343a133d20b87c7da32c83d4a80e3aee
Kunalsinh Solanki <99866867+Kunalsinh@users.noreply.github.com>
2024-01-15 13:18:33 +0530

    Python Practice Set 2 work on 15/01/2024

---
+++ pracSet2.py
@@ -0,0 +1,45 @@
+#Program 1 : Find Area of the Triangle
+
+'''
+baseOfT = float(input('Enter Base of Triangle :'))
+heightOfT = float(input('Enter Height of Triangle :'))
+
+areaTriangle = ((baseOfT * heightOfT) / 2)
+print('Area of Triangle :', areaTriangle)
+'''
+
+#Program 2 : Find Area of Square 
+
+'''
+heightS = float(input('Enter Height of Square :'))
+widthS = float(input('Enter Width of Square :'))
+
+areaSquare = heightS * widthS
+print('Area of Square :',areaSquare)
+'''
+
+#Program 3 : Write a program to convert Celcius to Ferenhit.
+
+'''
+celsiusNumber = float(input('Enter Celcius Number :'))
+convertedFahrenheit = (celsiusNumber *  (9/5) + 32) 
+print('Celsius into Fahrenheit :',convertedFahrenheit)
+'''
+
+#Program 4: Convert US Dollar  to Indian Rupees 
+
+'''
+indianRuppe = 82.88
+usDollarR = int(input('Enter US Dollar :'))
+convertedRupees = (usDollarR * indianRuppe)
+print('US Dollar convertetd to Indian Rupees :',convertedRupees)
+'''
+
+#Program 5 :Convert Litter to MiliLitter
+
+'''
+Litter = float(input('Enter How much Littler you want to be convertetd into MiliLitter :'))
+MiliLitter = 1000
+convertedML = (Litter * MiliLitter)
+print('Litter to MiliLitter',convertedML)
+'''
